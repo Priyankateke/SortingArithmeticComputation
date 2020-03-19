aadc1396881a3c8232c627ca35306f0aeaac101c
Priyankateke <priiyankateke1705@gmail.com>
2020-03-19 13:28:19 +0530

    ADDED: Arithmetic Operation 2

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -4,3 +4,4 @@ echo "Welcome To Arithmetic Computation And Sorting Problems"
 read -p "Enter Three Numbers : " a b c
 
 expression1_result=$(echo "scale=2; $a+$b*$c" | bc )
+expression2_result=$(echo "scale=2; $a*$b+$c" | bc )
