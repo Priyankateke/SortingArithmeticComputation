0f7513f3343cd7a4186c64dbb6027c3fb77d582a
Priyankateke <priiyankateke1705@gmail.com>
2020-03-19 14:07:56 +0530

    ADDED: Stored Expressions Result In Dictionary

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome To Arithmetic Computation And Sorting Problems"
+
+declare -A expressionsResult
+
 read -p "Enter Three Numbers : " a b c
 
-expression1_result=$(echo "scale=2; $a+$b*$c" | bc )
-expression2_result=$(echo "scale=2; $a*$b+$c" | bc )
-expression3_result=$(echo "scale=2; $c+$a/$b" | bc )
-expression4_result=$(($a%$b+$c))
+expressionsResult[expression1_result]=$(echo "scale=2; $a+$b*$c" | bc )
+expressionsResult[expression2_result]=$(echo "scale=2; $a*$b+$c" | bc )
+expressionsResult[expression3_result]=$(echo "scale=2; $c+$a/$b" | bc )
+expressionsResult[expression4_result]=$(($a%$b+$c))
